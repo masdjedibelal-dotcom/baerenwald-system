d264e028dddfd187fc71a218e69e99e4650e91c5
masdjedibelal-dotcom <masdjedibelal@gmail.com>
2026-06-04 20:49:36 +0200

    Delete supabase directory

--- supabase/migrations/20260525120000_preislisten_nur_preis_min.sql
+++
@@ -1,6 +0,0 @@
--- Preisliste: nur noch ein Preis (preis_min). preis_max entfällt.
-UPDATE preislisten
-SET preis_min = COALESCE(NULLIF(preis_min, 0), preis_max, 0)
-WHERE COALESCE(preis_min, 0) = 0 AND COALESCE(preis_max, 0) > 0;
-
-ALTER TABLE preislisten DROP COLUMN IF EXISTS preis_max;
