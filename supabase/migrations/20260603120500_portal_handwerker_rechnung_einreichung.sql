00f9f801bd9d2a464b558e43313936141f2065df
masdjedibelal-dotcom <masdjedibelal@gmail.com>
2026-06-05 23:26:57 +0200

    Delete supabase directory

--- supabase/migrations/20260603120500_portal_handwerker_rechnung_einreichung.sql
+++
@@ -1,8 +0,0 @@
--- Partner-Portal: Handwerker lädt Rechnungs-PDF hoch (nach Angebotseinreichung)
-
-alter table public.angebot_handwerker
-  add column if not exists hw_rechnung_pdf_url text,
-  add column if not exists hw_rechnung_eingereicht_at timestamptz;
-
-comment on column public.angebot_handwerker.hw_rechnung_pdf_url is 'Rechnungs-PDF (Storage handwerker-uploads)';
-comment on column public.angebot_handwerker.hw_rechnung_eingereicht_at is 'Zeitpunkt Rechnungs-Upload durch Handwerker';
