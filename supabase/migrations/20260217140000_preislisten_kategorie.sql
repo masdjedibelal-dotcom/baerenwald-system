aad79801da4666e352c2335b43a31f09d3bbcd31
masdjedibelal-dotcom <masdjedibelal@gmail.com>
2026-07-08 00:21:24 +0200

    Delete supabase directory

--- supabase/migrations/20260217140000_preislisten_kategorie.sql
+++
@@ -1,5 +0,0 @@
--- Kategorie für Preislisten-Gruppierung (UI: Tabs → Kategorie-Blöcke)
-alter table preislisten
-  add column if not exists kategorie text not null default '';
-
-comment on column preislisten.kategorie is 'Gruppierung innerhalb eines Gewerks, z. B. Komplettsanierung';
